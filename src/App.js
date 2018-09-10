3b6da125c43ce22d5e3ac102efbdd6a9e7924faa
Remi Kristelijn <remi.kristelijn@accenture.com>
2018-09-10 14:20:24 +0200

    college-32

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  // render = () => (
-  //   <div className="App">
-  //     <h1>Hi, I'm a React App</h1>
-  //   </div>
-  // );
+  render = () => (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <Person />
+    </div>
+  );
   // we need to import React, because under the hood is is used to compile the nested HTML, JSX
   // Alternatively you can use React.createElement('div', {className:'App'}, React.createElement('h1',null,))
-  render = () => React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'hello world'));
+  //render = () => React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'hello world'));
 };
 
 export default App;
