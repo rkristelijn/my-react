6f9345d115d10dadc77eaf7c76c9bb02a2bfa6f1
Remi Kristelijn <remi.kristelijn@accenture.com>
2018-09-10 15:46:01 +0200

    college-38

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  //state is part of Component
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stepanie', age: 26 },
+    ]
+  }
   render = () => (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
-      <Person name="Max" age="28" />
-      <Person name="Manu" age="29">My Hobbies: Racing</Person>
-      <Person name="Stephanie" age="26" />
-      <Person />
-      <Person />
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      <button>Switch name</button>
     </div>
   );
   // we need to import React, because under the hood is is used to compile the nested HTML, JSX
