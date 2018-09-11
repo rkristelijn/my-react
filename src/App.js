8e7439135fca250997d0a68bd25023259c951813
Remi Kristelijn <remi.kristelijn@accenture.com>
2018-09-11 10:27:17 +0200

    college-45

--- src/App.js
+++ src/App.js
@@ -14,16 +14,30 @@ class App extends Component {
 
   switchNameHandler = (newName) => {
     //this.state.persons[0].name = 'Maximilian'; //Line 16:  Do not mutate state directly. Use setState()  react/no-direct-mutation-state
-    this.setState({persons:[
-      { name: newName, age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 27 },
-    ]});
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ]
+    });
   } // LOG: was clicked App {}
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ]
+    });
+  }
 
   // switchNameHandler() {
   //   console.log('was clicked', this);
   // } // LOG: was clicked undefined -> so with old syntax, 'this' is undefined
+
+
   // note: switchNameHandler can be passed as a method in props
   // bind(this, newValue) let's you pass values
   // alternatively you can use () => this.switchNameHandler('Blaaa'), but bind is preferred, because this is inefficient (performance)
@@ -31,15 +45,15 @@ class App extends Component {
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={() => this.switchNameHandler('Blaaa')} />
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} changed={this.nameChangedHandler} />
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler} click={this.switchNameHandler.bind(this, 'Remi')} />
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} changed={this.nameChangedHandler}/>
       <button onClick={this.switchNameHandler.bind(this, 'Remi')}>Switch name</button>
     </div>
   );
   // we need to import React, because under the hood is is used to compile the nested HTML, JSX
   // Alternatively you can use React.createElement('div', {className:'App'}, React.createElement('h1',null,))
-  //render = () => React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'hello world'));
+  // render = () => React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'hello world'));
 };
 
 export default App;
