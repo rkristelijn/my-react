cee7e6d67b46edceeaff13810c3a08d241a5bb44
Remi Kristelijn <remi.kristelijn@accenture.com>
2018-09-12 10:38:59 +0200

    college-65 media selectors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -81,7 +81,7 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
+      ':hover': { //hover is a css pseudo selector
         backgroundColor: 'lightgreen',
         color: 'black'
       }
@@ -115,12 +115,14 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons {this.state.showPersons}</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons {this.state.showPersons}</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   };
   // we need to import React, because under the hood is is used to compile the nested HTML, JSX
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,14 @@ import Radium from 'radium';
 import './Person.css';
 
 const person = (props) => {
-    return (<div className="Person">
+    //Uncaught Error: To use plugins requiring `addCSS` (e.g. keyframes, media queries), please wrap your application in the StyleRoot component. Component name: `person`.
+    // solution: wrap full app in <StyleRoot>
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    }
+    return (<div className="Person" style={style}>
         <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name} />
